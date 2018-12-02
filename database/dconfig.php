6d1574710c59e6f2c56c75874cc9293e709ce5de
nlgncnbzz <35635357+nlgncnbzz@users.noreply.github.com>
2018-12-02 20:33:32 +0300

    Add files via upload

---
+++ database/dconfig.php
@@ -0,0 +1,19 @@
+<?php
+	  
+		$server="localhost";
+		$kullaniciAdi="root";
+		$sifre = "";
+
+
+			try{
+				$database_connection = new PDO("mysql:host=$server;dbname=ev-gelir-gider;charset=utf8",$kullaniciAdi,$sifre);
+				$database_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				//echo" Baglanti basarili..";
+			}catch(PDOException $e){
+
+				echo $e->getMessage();
+			}
+	
+?>
+
+
